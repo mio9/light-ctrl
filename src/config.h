98aae9d48ffbc781436c3976de1cc601060a7f58
Mio Nekoya <admin@mio9.sh>
2024-08-13 11:47:30 +0900

    separate out config

---
+++ src/config.h
@@ -0,0 +1,11 @@
+// IO Pins
+#define LED_BOARD_DATA_PIN GPIO_NUM_1
+
+// OLED things
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+
+// LED things
+#define NUM_LEDS 64 // 256*3
--- src/main.cpp
+++ src/main.cpp
@@ -11,23 +11,14 @@
 // the main led
 #include <FastLED.h>
 
+#include "config.h"
+
 void oledCheck();
 
 void ledScroller(void *parameters);
 
-// IO Pins
-#define LED_BOARD_DATA_PIN GPIO_NUM_1
-
-// OLED things
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-// LED things
-#define NUM_LEDS 64 // 256*3
 CRGB leds[NUM_LEDS];
 
 void someTask(void *parameters)
